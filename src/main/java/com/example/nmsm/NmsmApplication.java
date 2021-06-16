2e4994f186bdeada312d78557b7345aaac09c7c5
Kimsw-G <kimsw3445@naver.com>
2021-06-16 09:17:41 +0900

    Initial commit

---
+++ src/main/java/com/example/nmsm/NmsmApplication.java
@@ -0,0 +1,13 @@
+package com.example.nmsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmsmApplication.class, args);
+    }
+
+}
