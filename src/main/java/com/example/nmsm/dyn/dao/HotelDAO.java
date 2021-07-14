57fa30c7b8c411cc0bb5ba8d5b978a05a6bb1c97
ellenlim01 <ellenlim02@naver.com>
2021-07-14 16:23:28 +0900

    insert dog breed

--- src/main/java/com/example/nmsm/dyn/dao/HotelDAO.java
+++ src/main/java/com/example/nmsm/dyn/dao/HotelDAO.java
@@ -1,4 +1,24 @@
 package com.example.nmsm.dyn.dao;
 
+import com.example.nmsm.sta.model.BookInfoEntity;
+import com.example.nmsm.sta.model.HotelInfoEntity;
+import com.example.nmsm.sta.model.HotelReviewEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface HotelDAO {
+    List<HotelInfoEntity> getHotelList(BookInfoEntity bookInfoEntity);
+    HotelInfoEntity getHotelInfo(int ihotel);
+    List<HotelReviewEntity> getHotelReview(int ihotel);
+
+    void insHotelReview(HotelReviewEntity hotelReviewEntity);
+    void insHotelReviewImg(String reviewImg);
+
+    void updHotelReview(HotelReviewEntity hotelReviewEntity);
+
+    void delHotelReview(int ireview);
+
+
 }
--- src/main/java/com/example/nmsm/dyn/service/CommonService.java
+++ src/main/java/com/example/nmsm/dyn/service/CommonService.java
@@ -3,6 +3,7 @@ package com.example.nmsm.dyn.service;
 
 import com.example.nmsm.sta.config.auth.PrincipalDetails;
 
+
 public class CommonService {
 
     public int getIuser(PrincipalDetails principalDetails){
