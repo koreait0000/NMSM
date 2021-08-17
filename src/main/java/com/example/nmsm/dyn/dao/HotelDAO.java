93d5476b11f7b6862b21e316b2db306873be6a55
Kimsw-G <kimsw3445@naver.com>
2021-08-17 15:34:44 +0900

    finish enter service

--- src/main/java/com/example/nmsm/dyn/dao/HotelDAO.java
+++ src/main/java/com/example/nmsm/dyn/dao/HotelDAO.java
@@ -2,10 +2,12 @@ package com.example.nmsm.dyn.dao;
 
 
 import com.example.nmsm.sta.model.HotelInfoEntity;
+import com.example.nmsm.sta.model.HotelServiceEntity;
 import com.example.nmsm.sta.model.dto.HotelInfoDTO;
 import com.example.nmsm.sta.model.BookInfoEntity;
 import com.example.nmsm.sta.model.dto.HotelReviewDTO;
 
+import com.example.nmsm.sta.model.dto.HotelServiceDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -17,19 +19,24 @@ public interface HotelDAO {
     HotelInfoDTO selHotelInfo(BookInfoEntity bookInfoEntity);
     List<HotelReviewDTO> selHotelReview(BookInfoEntity bookInfoEntity);
     List<HotelInfoDTO> selectLikeHotelInfoByIuser(int iuser);
-    HotelInfoDTO selectMyHotelInfoByIuser(int iuser);
+    HotelInfoDTO    selectMyHotelInfoByIuser(int iuser);
+    int selectHPkByIuser(int iuser);
 
     int insHotelReview(HotelReviewDTO hotelReviewDTO);
     int insHotelReviewImg(HotelReviewDTO hotelReviewDTO);
     int insAvgStar(int ihotel);
 
-    void insertHotel(HotelInfoEntity hotelInfoEntity);
+    int insertHotel(HotelInfoEntity hotelInfoEntity);
+    int insertService(List<HotelServiceEntity> hotelServiceDTOS);
 
     int updHotelReview(HotelReviewDTO hotelReviewDTO);
     int updHotelReviewImg(HotelReviewDTO hotelReviewDTO);
+    int updateHotelInfo(HotelInfoEntity hotelInfoEntity);
 
     int delHotelReview(HotelReviewDTO hotelReviewDTO);
     int delHotelReviewImg(HotelReviewDTO hotelReviewDTO);
+    int deleteService(int ihotel);
+
 
     int selReviewNum(int ihotel);
     int selMaxPageVal(String hLocation, int recordCnt);
